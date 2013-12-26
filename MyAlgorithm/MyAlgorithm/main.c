59a6cb012e476e5402ff2b66a7282470309d27b4
liaocpen <wcpen@qq.com>
2013-12-26 18:27:04 +0800

    no message

---
+++ MyAlgorithm/MyAlgorithm/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyAlgorithm
+//
+//  Created by lanhu on 13-12-26.
+//  Copyright (c) 2013年 lanhu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
