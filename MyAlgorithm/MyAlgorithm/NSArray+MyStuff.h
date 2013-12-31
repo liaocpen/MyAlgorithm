dbd4070de2ee93fd1aa6f8ba7ea2112ccec2631f
liaocpen <wcpen@qq.com>
2013-12-31 18:32:07 +0800

    no message

--- MyAlgorithm/MyAlgorithm/NSArray+MyStuff.h
+++ MyAlgorithm/MyAlgorithm/NSArray+MyStuff.h
@@ -10,5 +10,27 @@
 
 @interface NSArray (MyStuff)
 
+/**
+ *  获取数组中最大值的索引
+ *
+ *  @return index of maximum element in array
+ */
 - (NSUInteger)indexOfMaximumElement;
+
+/**
+ *  Get the Max and Min Simultaneously.
+ *  @note Assumes array of NSInteger values
+ *  @return NSArray with two elements: indexes of elements with
+        minimal/maximal values respectively or nil if receiver array is empty
+ */
+- (NSArray *)indexesOfMinimumAndMaximumElements;
+
+- (NSString *)longestString;
+- (NSString *)shortestString;
+
+- (NSArray *)reversedArray;
+- (NSArray *)CocoaImplementationOfReversedArray;
+
+- (NSArray *)intersectionWithArray:(NSArray *)secondArray;
+
 @end
--- MyAlgorithm/MyAlgorithm/main.m
+++ MyAlgorithm/MyAlgorithm/main.m
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
+///
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
